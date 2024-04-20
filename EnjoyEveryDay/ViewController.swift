01c2dc04cdb5f477d8f4e9bbe7002cfd6e579999
Daniil <sivozelezovdaniil@gmail.com>
2024-04-20 22:38:26 +0500

    MyDays
    MyDays

--- EnjoyEveryDay/ViewController.swift
+++ EnjoyEveryDay/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var resultButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
-    private var numberOfDays = ""
+    var numberOfDays = ""
     @IBOutlet weak var datePicker: UIDatePicker!
     
     override func viewDidLoad() {
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     
     @IBAction func datePicker(_ sender: UIDatePicker) {
         let range = sender.date..<Date.now
-        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]).locale(Locale(identifier: "ru_RU")))
     }
     
     @IBAction func resultButtonTapped() {
